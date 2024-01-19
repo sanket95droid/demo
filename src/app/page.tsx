ccedebfaa0b9eb4db67d5840b7d14624fbc91089
sanket95droid <sanketzambre1@gmail.com>
2024-01-19 14:12:46 +0530

    setup done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,9 +2,9 @@ import  {MaxWidthWrapper} from "./component/MaxWidthWrapper"
 
 export default function Home() {
   return (
-    <MaxWidthWrapper className="bg-red">
-      <div>
-
+    <MaxWidthWrapper className="mb-12 mt-28 sm:mt-40 flex flex-col items-center justify-center text-center">
+      <div className="mx-auto mb-4 flex max-w-fit items-center justify-center space-x-2 overflow-hidden rounded-full border border-gray-200 bg-white px-7 py-2 shodow-md backdrop-blur transition-all hover:border-gray-300 hover:bg-white/50">
+        hello world
       </div>
     </MaxWidthWrapper>
   )
